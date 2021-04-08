790bab376fd8650d1ddb3e1aea189afb7a26f56b
LahaLuhem <t3mp0rary13@gmail.com>
2021-04-08 14:16:05 +0200

    added more methods

--- python_reborn/Tuple.java
+++ python_reborn/Tuple.java
@@ -1,8 +1,12 @@
 package python_reborn;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Enables a similar-ish functionality of the dynamic-type Tuple object object in languages like Python
  * into a verbose language such as Java.
+ * This Tuple is mutable
  * @author LahaLuhem
  * @version 1.0.0
  */
@@ -11,6 +15,10 @@ public class Tuple<T> {
 
     Tuple (T ... objs) {
         elems = objs;
+    }
+    
+    Tuple () {
+        elems = null;
     }
 
     @Override
@@ -27,8 +35,54 @@ public class Tuple<T> {
     }
     
     
+    /**
+     * Return the length of the tuple
+     * @return The length
+     */
     public int len (){
         return elems.length;
+    }
+    
+    /**
+     * Returns the element at a given index. Supports negative indexing
+     * @param index The index of the element to be retrieved
+     * @return The element at given index
+     */
+    public T get (int index) {
+        return elems[ ((index < 0)? elems.length: 0) + index ];
+    }
+    
+    /**
+     * Allows changing of individual elements, only if the new one is of the same type
+     * Does not throw an Exception if no the case
+     * @param index The element to be modified
+     * @param newElem The value to be modified with
+     */
+    public void setAt (int index, T newElem) {
+        if (newElem.getClass().equals(elems[index].getClass())) elems[index] = newElem;
+    }
+    
+    /**
+     * Slices the tuple by the given range
+     * @param start Starting index of the new slice
+     * @param end Ending index of the new slice
+     * @return A new sliced Tuple
+     */
+    public Tuple slice (int start, int end) {
+        return new Tuple ( Arrays.copyOfRange(elems, start, end) );
+    }
+    
+    /**
+     * TO BE IMPLEMENTED PROPERLY
+     * @param tup2
+     * @return 
+     */
+    public Tuple concat (Tuple tup2) {
+        List<T> tup1List = Arrays.asList(elems);
+        for (Object elem : tup2.elems)
+            tup1List.add((T) elem);
+        
+        return new Tuple (tup1List);
     }
     
     /**
